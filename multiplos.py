067e5c814ab21aa9a8fcf7a2a4f9a10bf75ef7d2
Noreth29 <noreth.guajardo@iteso.mx>
2025-02-18 08:01:22 -0600

    prueba 2

--- multiplos.py
+++ multiplos.py
@@ -9,15 +9,20 @@ try:
     numero1 = int(input("Introduzca un número "))
     numero2= int(input("Introduzca otro número "))
 
+    #if numero1 == 0 or numero2 == 0:
+       # print("Prueba con otro número ")
 # Proceso
     if numero1 % numero2 == 0:
         print("El número " + str(numero1) + " es múltiplo del " + str(numero2))
 
     elif numero2 % numero1 == 0:
-       print("El número " + str(numero2) + " es múltiplo del " + str(numero1)) 
+        print("El número " + str(numero2) + " es múltiplo del " + str(numero1)) 
 
     else:
-       print("Ningún número es múltiplo del otro")
+        print("Ninguno de los números es múltiplo del otro ")
 
-except ValueError:
+except ZeroDivisionError :
     print("Error. Introduzca un número válido")
+
+except ValueError :
+    print("Error. Introduzca otro valor.")
