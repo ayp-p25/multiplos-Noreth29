91a5da720e95547432578bcdee4e89ce4dba2c37
Noreth29 <noreth.guajardo@iteso.mx>
2025-02-17 20:35:34 -0600

    primer intento

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,23 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+multiplos
 """
 
 # Declaraciones
-CONSTANTE = valor
+try:
 
 # Entradas
-entrada = input()
+    numero1 = int(input("Introduzca un número "))
+    numero2= int(input("Introduzca otro número "))
 
 # Proceso
+    if numero1 % numero2 == 0:
+        print("El número " + str(numero1) + " es múltiplo del " + str(numero2))
 
+    elif numero2 % numero1 == 0:
+       print("El número " + str(numero2) + " es múltiplo del " + str(numero1)) 
 
-# Salidas
-print(salida)
+    else:
+       print("Ningún número es múltiplo del otro")
+
+except ValueError:
+    print("Error. Introduzca un número válido")
